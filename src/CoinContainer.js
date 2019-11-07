0de50a69f08af0275f09afc8ee89d34badf282d3
Codebrother1 <terrance.cazy@me.com>
2019-11-07 14:32:02 -0600

    add state and currcoin flip action

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import { choice } from "./helpers";
 
 class CoinContainer extends Component {
   static defaultProps = {
     coins: [
-      { side: "head", imgSrc: "https://tinyurl.com/react-coin-heads-jpg" },
-      { side: "tails", imgSrc: "https://tinyurl.com/react-coin-tail-jpg" }
+      { side: "heads", imgSrc: "https://tinyurl.com/react-coin-heads-jpg" },
+      { side: "tails", imgSrc: "https://tinyurl.com/react-coin-tails-jpg" }
     ]
   };
 
@@ -16,13 +17,29 @@ class CoinContainer extends Component {
       nHeads: 0,
       nTails: 0
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  flipCoin() {
+    const newCoin = choice(this.props.coins);
+    this.setState(st => {
+      return {
+        currCoin: newCoin,
+        nFlips: st.nFlips + 1,
+        nHeads: st.nHeads + (newCoin.side === "heads" ? 1 : 0),
+        nTails: st.nTails + (newCoin.side === "tails" ? 1 : 0)
+      };
+    });
+  }
+  handleClick(e) {
+    this.flipCoin();
   }
   render() {
     return (
       <div className="CoinContainer">
         <h2>Let's flip a coin!</h2>
+        <button onClick={this.handleClick}>Flip Me!</button>
         <p>
-          Out of {this.state.nFlips} flips, there has been {this.state.nHeads}
+          Out of {this.state.nFlips} flips, there has been {this.state.nHeads}{" "}
           heads and {this.state.nTails} tails.
         </p>
       </div>
